569a719e90c27269e87f314fbd23436945ab2230
abdellahfihri <abdellahfihri@gmail.com>
2021-02-23 10:43:38 +0100

    first commit

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+})
+export class MessagesComponent implements OnInit {
+  constructor(public messageService: MessageService) {} // must be public if we want to bind it to template
+
+  ngOnInit(): void {}
+}
